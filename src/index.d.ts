e9bc31fe546aaf5350663f869398c24362e0cb6f
Samual Norman <me@samual.uk>
2026-02-26 13:50:12 +0000

    make sure `Options` is also directly exported to match behaviour of `rollup-plugin-prettier`

--- src/index.d.ts
+++ src/index.d.ts
@@ -5,23 +5,25 @@
 import type { Options as PrettierOptions } from 'prettier';
 import type { Plugin } from 'rolldown';
 
-declare namespace prettier {
-  interface Options extends PrettierOptions {
-    /**
-     * Directory to look for a Prettier config file.
-     *
-     * If omitted, defaults to `process.cwd()`.
-     */
-    cwd?: string;
+export interface Options extends PrettierOptions {
+  /**
+   * Directory to look for a Prettier config file.
+   *
+   * If omitted, defaults to `process.cwd()`.
+   */
+  cwd?: string;
 
-    /**
-     * Whether to generate a sourcemap.
-     *
-     * Note: This may take some time because rollup-plugin-prettier diffs the
-     * output to manually generate a sourcemap.
-     */
-    sourcemap?: boolean | 'silent';
-  }
+  /**
+   * Whether to generate a sourcemap.
+   *
+   * Note: This may take some time because rollup-plugin-prettier diffs the
+   * output to manually generate a sourcemap.
+   */
+  sourcemap?: boolean | 'silent';
+}
+
+declare namespace prettier {
+ export { Options }
 }
 
 declare function prettier(options?: prettier.Options): Plugin;
