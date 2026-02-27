e41db11f310a2ebd28e0baa27bf99d4d3b167157
Samual Norman <me@samual.uk>
2026-02-27 10:04:02 +0000

    style

--- scripts/emit-dts.sh
+++ scripts/emit-dts.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 set -eux
-node_modules/.bin/tsc -p src --noEmit false --outDir dist --emitDeclarationOnly true --declaration true --noCheck
+node_modules/.bin/tsc --project src --noEmit false --outDir dist --emitDeclarationOnly true --declaration true --noCheck
