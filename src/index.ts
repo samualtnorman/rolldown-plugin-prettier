09f4ea5f5f5c48de92c5ded3798f862466d34f43
Samual Norman <me@samual.uk>
2026-03-07 22:54:52 +0000

    convert indentation to tabs

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,24 @@ import type { Plugin } from "rolldown"
 import type { Options as PrettierOptions } from "prettier"
 
 export interface Options extends PrettierOptions {
-  /**
-   * Directory to look for a Prettier config file.
-   *
-   * If omitted, defaults to `process.cwd()`.
-   */
-  cwd?: string
+	/**
+	 * Directory to look for a Prettier config file.
+	 *
+	 * If omitted, defaults to `process.cwd()`.
+	 */
+	cwd?: string
 
-  /**
-   * Whether to generate a sourcemap.
-   *
-   * Note: This may take some time because rollup-plugin-prettier diffs the
-   * output to manually generate a sourcemap.
-   */
-  sourcemap?: boolean | "silent"
+	/**
+	 * Whether to generate a sourcemap.
+	 *
+	 * Note: This may take some time because rollup-plugin-prettier diffs the
+	 * output to manually generate a sourcemap.
+	 */
+	sourcemap?: boolean | "silent"
 }
 
 declare namespace rollupPluginPrettier {
-  export { Options }
+	export { Options }
 }
 
 /**
@@ -30,29 +30,29 @@ declare namespace rollupPluginPrettier {
  * @return Plugin instance.
  */
 function rollupPluginPrettier(options: Options): Plugin {
-  const plugin = new RollupPluginPrettier(options)
+	const plugin = new RollupPluginPrettier(options)
 
-  return {
-    /**
-     * Plugin name (used by rollup for error messages and warnings).
-     * @type {string}
-     */
-    name: plugin.name,
+	return {
+		/**
+		 * Plugin name (used by rollup for error messages and warnings).
+		 * @type {string}
+		 */
+		name: plugin.name,
 
-    /**
-     * Function called by `rollup` before generating final bundle.
-     *
-     * @param source Souce code of the final bundle.
-     * @param _chunkInfo Chunk info.
-     * @param outputOptions Output option.
-     * @return The result containing a `code` property and, if a enabled, a `map` property.
-     */
-    renderChunk(source, _chunkInfo, outputOptions) {
-      return plugin.reformat(source, {
-        sourcemap: Boolean(outputOptions.sourcemap)
-      })
-    }
-  }
+		/**
+		 * Function called by `rollup` before generating final bundle.
+		 *
+		 * @param source Souce code of the final bundle.
+		 * @param _chunkInfo Chunk info.
+		 * @param outputOptions Output option.
+		 * @return The result containing a `code` property and, if a enabled, a `map` property.
+		 */
+		renderChunk(source, _chunkInfo, outputOptions) {
+			return plugin.reformat(source, {
+				sourcemap: Boolean(outputOptions.sourcemap)
+			})
+		}
+	}
 }
 
 export { rollupPluginPrettier as default }
