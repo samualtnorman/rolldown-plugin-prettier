2ed92b256e403b18615de50c2dd9c53d79c385cb
Samual Norman <me@samual.uk>
2026-03-07 21:44:03 +0000

    simplify

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,9 +1,8 @@
 #!node_modules/.bin/rolldown --config
 import { rolldownConfig } from "@samual/rolldown-config"
-import * as Path from "path"
 
 export default rolldownConfig({
 	rolldownOptions: {
-		external: source => !(source == `es-toolkit` || source.startsWith(`es-toolkit/`) || Path.isAbsolute(source) || source.startsWith("."))
+		external: source => !(source == "es-toolkit" || source.startsWith("es-toolkit/") || source[0] == `/` || source[0] == ".")
 	}
 })
