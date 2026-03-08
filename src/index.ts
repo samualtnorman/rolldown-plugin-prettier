546ec9b19be589348a06b4818fe44ac9df94bb80
Samual Norman <me@samual.uk>
2026-03-08 00:35:23 +0000

    refactor away `rollup-plugin-prettier.ts` by finally merging it into `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { NAME, reformat } from "./rollup-plugin-prettier"
-import type { Plugin } from "rolldown"
+import * as diff from "diff"
+import MagicString from "magic-string"
+import path from "node:path"
 import type { Options as PrettierOptions } from "prettier"
+import prettier from "prettier"
+import type { Plugin } from "rolldown"
+
+export const NAME = `rolldown-plugin-prettier`
 
 export interface Options extends PrettierOptions {
 	/**
@@ -45,10 +50,55 @@ function rollupPluginPrettier(options: Options): Plugin {
 		 * @param outputOptions Output option.
 		 * @return The result containing a `code` property and, if a enabled, a `map` property.
 		 */
-		renderChunk(source, _chunkInfo, outputOptions) {
-			return reformat(options, source, {
-				sourcemap: Boolean(outputOptions.sourcemap)
-			})
+		async renderChunk(source, _chunkInfo, outputOptions) {
+			// Since prettier 3.1.1, the resolver searches from a file path, not a directory.
+			// Let's fake it by concatenating with a file name.
+			const prettierConfig = await prettier.resolveConfig(path.join(options.cwd ?? process.cwd()))
+			const { sourcemap, cwd, ...prettierOptions } = options
+			const mergedOptions = { ...prettierConfig, ...prettierOptions }
+	
+			const output = await prettier.format(
+				source,
+				Reflect.ownKeys(mergedOptions).length ? mergedOptions : undefined
+			)
+
+			// Should we generate sourcemap?
+			// The sourcemap option may be a boolean or any truthy value (such as a `string`).
+			// Note that this option should be false by default as it may take a (very) long time.
+			const defaultSourcemap = options.sourcemap ?? false;
+			if (!(outputOptions?.sourcemap ?? defaultSourcemap)) {
+				return { code: output };
+			}
+
+			if (defaultSourcemap !== 'silent') {
+				console.warn(`[${NAME}] Sourcemap is enabled, computing diff is required`);
+				console.warn(`[${NAME}] This may take a moment (depends on the size of your bundle)`);
+			}
+
+			const magicString = new MagicString(source);
+			const changes = diff.diffChars(source, output);
+
+			if (changes && changes.length > 0) {
+				let idx = 0;
+
+				changes.forEach((part) => {
+					if (part.added) {
+						magicString.prependLeft(idx, part.value);
+						idx -= part.count;
+					} else if (part.removed) {
+						magicString.remove(idx, idx + part.count);
+					}
+
+					idx += part.count;
+				});
+			}
+
+			return {
+				code: magicString.toString(),
+				map: magicString.generateMap({
+					hires: true,
+				}),
+			};
 		}
 	}
 }
