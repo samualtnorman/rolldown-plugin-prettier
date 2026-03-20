36ffd6ce4b02e2a17114f9390cbab40594b644cd
Samual Norman <me@samual.uk>
2026-03-20 14:16:43 +0000

    make the plugin function an arrow function

--- src/index.ts
+++ src/index.ts
@@ -34,72 +34,70 @@ declare namespace rollupPluginPrettier {
  * @param options Plugin options.
  * @return Plugin instance.
  */
-function rollupPluginPrettier(options: Options): Plugin {
-	return {
-		/**
-		 * Plugin name (used by rollup for error messages and warnings).
-		 * @type {string}
-		 */
-		name: NAME,
+const rollupPluginPrettier = (options: Options): Plugin => ({
+	/**
+	 * Plugin name (used by rollup for error messages and warnings).
+	 * @type {string}
+	 */
+	name: NAME,
 
-		/**
-		 * Function called by `rollup` before generating final bundle.
-		 *
-		 * @param source Souce code of the final bundle.
-		 * @param _chunkInfo Chunk info.
-		 * @param outputOptions Output option.
-		 * @return The result containing a `code` property and, if a enabled, a `map` property.
-		 */
-		async renderChunk(source, _chunkInfo, outputOptions) {
-			// Since prettier 3.1.1, the resolver searches from a file path, not a directory.
-			// Let's fake it by concatenating with a file name.
-			const prettierConfig = await prettier.resolveConfig(path.join(options.cwd ?? process.cwd()))
-			const { sourcemap, cwd, ...prettierOptions } = options
-			const mergedOptions = { ...prettierConfig, ...prettierOptions }
-	
-			const output = await prettier.format(
-				source,
-				Reflect.ownKeys(mergedOptions).length ? mergedOptions : undefined
-			)
+	/**
+	 * Function called by `rollup` before generating final bundle.
+	 *
+	 * @param source Souce code of the final bundle.
+	 * @param _chunkInfo Chunk info.
+	 * @param outputOptions Output option.
+	 * @return The result containing a `code` property and, if a enabled, a `map` property.
+	 */
+	async renderChunk(source, _chunkInfo, outputOptions) {
+		// Since prettier 3.1.1, the resolver searches from a file path, not a directory.
+		// Let's fake it by concatenating with a file name.
+		const prettierConfig = await prettier.resolveConfig(path.join(options.cwd ?? process.cwd()))
+		const { sourcemap, cwd, ...prettierOptions } = options
+		const mergedOptions = { ...prettierConfig, ...prettierOptions }
 
-			// Should we generate sourcemap?
-			// The sourcemap option may be a boolean or any truthy value (such as a `string`).
-			// Note that this option should be false by default as it may take a (very) long time.
-			if (!outputOptions.sourcemap) {
-				return { code: output };
-			}
+		const output = await prettier.format(
+			source,
+			Reflect.ownKeys(mergedOptions).length ? mergedOptions : undefined
+		)
 
-			if (options.sourcemap != 'silent') {
-				console.warn(`[${NAME}] Sourcemap is enabled, computing diff is required`);
-				console.warn(`[${NAME}] This may take a moment (depends on the size of your bundle)`);
-			}
-
-			const magicString = new MagicString(source);
-			const changes = diffChars(source, output);
-
-			if (changes.length) {
-				let idx = 0;
-
-				changes.forEach((part) => {
-					if (part.added) {
-						magicString.prependLeft(idx, part.value);
-						idx -= part.count;
-					} else if (part.removed) {
-						magicString.remove(idx, idx + part.count);
-					}
-
-					idx += part.count;
-				});
-			}
-
-			return {
-				code: magicString.toString(),
-				map: magicString.generateMap({
-					hires: true,
-				}),
-			};
+		// Should we generate sourcemap?
+		// The sourcemap option may be a boolean or any truthy value (such as a `string`).
+		// Note that this option should be false by default as it may take a (very) long time.
+		if (!outputOptions.sourcemap) {
+			return { code: output };
 		}
+
+		if (options.sourcemap != 'silent') {
+			console.warn(`[${NAME}] Sourcemap is enabled, computing diff is required`);
+			console.warn(`[${NAME}] This may take a moment (depends on the size of your bundle)`);
+		}
+
+		const magicString = new MagicString(source);
+		const changes = diffChars(source, output);
+
+		if (changes.length) {
+			let idx = 0;
+
+			changes.forEach((part) => {
+				if (part.added) {
+					magicString.prependLeft(idx, part.value);
+					idx -= part.count;
+				} else if (part.removed) {
+					magicString.remove(idx, idx + part.count);
+				}
+
+				idx += part.count;
+			});
+		}
+
+		return {
+			code: magicString.toString(),
+			map: magicString.generateMap({
+				hires: true,
+			}),
+		};
 	}
-}
+})
 
 export { rollupPluginPrettier as default, rollupPluginPrettier as prettier, rollupPluginPrettier as rolldownPluginPrettier }
